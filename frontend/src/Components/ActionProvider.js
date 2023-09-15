80277dfc5f1a163979e336a8f08ae393218313fb
aj1519 <144786074+aj1519@users.noreply.github.com>
2023-09-15 10:13:50 +0530

    Update ActionProvider.js

--- frontend/src/Components/ActionProvider.js
+++ frontend/src/Components/ActionProvider.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-  const handleInput = (message) => {
-    axios.get('http://127.0.0.1:5000/gpt?text_input=' + message).then((response) => {
+  const convertUrlToHtml = async (docxUrl) => {
+    try {
+      const response = await axios.get(docxUrl, { responseType: 'arraybuffer' });
+      const arrayBuffer = response.data;
+      const blob = new Blob([arrayBuffer], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+      const url = URL.createObjectURL(blob);
+      return url;
+    } catch (error) {
+      console.error('Error converting to HTML:', error);
+      return null;
+    }
+  };
+
+  const handleInput = async (message) => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:5000/gpt?text_input=${message}`);
       console.log(response.data['output_gpt']);
+
+      const docxUrl = response.data['document']; // Assuming this is the URL of the Word document @abhishek pls check this
+      const htmlUrl = await convertUrlToHtml(docxUrl);
+
       const botMessage = createChatBotMessage(response.data['output_gpt']['DesignDoc']['Intent'], {
         widget: 'umlRenderer',
-        payload: {"diagram": response.data['diagram'],"document": response.data['document']}
+        payload: { "diagram": response.data['diagram'], "document": htmlUrl }
       });
+
       setState((prev) => ({
         ...prev,
         messages: [...prev.messages, botMessage],
       }));
-    });
+    } catch (error) {
+      console.error('Error handling input:', error);
+    }
   };
 
   return (
